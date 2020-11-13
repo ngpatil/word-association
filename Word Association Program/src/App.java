8966371f4c3a42c6acd8617700ea19cd62f60042
ngpatil <npatil314@gmail.com>
2020-11-12 22:30:01 -0600

    All words based on starting word

--- Word Association Program/src/App.java
+++ Word Association Program/src/App.java
@@ -10,55 +10,59 @@ import java.io.File;
 import java.io.FileWriter;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+        public static void main(String[] args) throws Exception {
 
-        JOptionPane.showMessageDialog(null, "Welcome to the Word Assocation Program!", "WAP",
-                JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(null, "Welcome to the Word Assocation Program!", "WAP",
+                                JOptionPane.INFORMATION_MESSAGE);
 
-        // used to create a unique filename
-        String name = JOptionPane.showInputDialog(null, "Enter your last name:", "WAP",
-                JOptionPane.INFORMATION_MESSAGE);
+                // used to create a unique filename
+                String name = JOptionPane.showInputDialog(null, "Enter your last name:", "WAP",
+                                JOptionPane.INFORMATION_MESSAGE);
 
-        Integer[] ops = {1, 2};
-        int choice = (Integer) JOptionPane.showInputDialog(null, "Choose the group code specified by the administrator::", "Group Code", JOptionPane.QUESTION_MESSAGE, null, ops, ops[0]);
+                // starting word
+                String choice = JOptionPane.showInputDialog(null,
+                                "Enter the starting word specified by the administrator:", "WAP",
+                                JOptionPane.INFORMATION_MESSAGE);
 
-        // these are some default options, subject to change
-        Integer[] options = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
-        int wordLimit = (Integer) JOptionPane.showInputDialog(null, "Pick a word limit:", "Word Limit",
-                JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+                // these are some default options, subject to change
+                Integer[] options = { 5, 10, 15, 20, 25, 30, 35, 40, 45, 50 };
+                int wordLimit = (Integer) JOptionPane.showInputDialog(null, "Pick a word limit:", "Word Limit",
+                                JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 
-        JOptionPane.showMessageDialog(null, "Enter the first word that comes to your mind.", "WAP",
-                JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(null,
+                                "Enter words that come to your mind based on the word \"" + choice + "\".", "WAP",
+                                JOptionPane.INFORMATION_MESSAGE);
 
-        // inputting the first word and creating the WordList
-        String input = JOptionPane.showInputDialog(null, "Enter a starting word:", "WAP",
-                JOptionPane.INFORMATION_MESSAGE);
+                // inputting the first word and creating the WordList
+                String input = JOptionPane.showInputDialog(null, choice, "WAP", JOptionPane.INFORMATION_MESSAGE);
 
-        WordList wList = new WordList(input);
+                WordList wList = new WordList(input);
 
-        // it is important that they always see the previous word
-        String prev = input;
+                // it is important that they always see the previous word
+                String prev = input;
 
-        // read in the rest of the words
-        for (int i = 0; i < wordLimit - 1; i++) {
-            input = JOptionPane.showInputDialog(null, prev, "WAP", JOptionPane.INFORMATION_MESSAGE);
-            wList.addWord(input);
-            prev = input;
+                // read in the rest of the words
+                for (int i = 0; i < wordLimit - 1; i++) {
+                        input = JOptionPane.showInputDialog(null, choice, "WAP", JOptionPane.INFORMATION_MESSAGE);
+                        wList.addWord(input);
+                        prev = input;
+                }
+
+                // writing to file
+                File file = new File(name + "-" + choice + "-" + wList.getLength() + ".txt");
+                file.createNewFile();
+                FileWriter writer = new FileWriter(file);
+                writer.write("WAP Results for " + name + ":\n\n");
+                writer.write("Starting word: "+choice+"\n\n");
+                writer.write(wList.toString());
+                writer.flush();
+                writer.close();
+
+                // exit message
+                JOptionPane.showMessageDialog(null,
+                                "Your results have been saved to " + name + "-" + choice + "-" + wList.getLength()
+                                                + ".txt.\nThank you for participating!",
+                                "WAP", JOptionPane.INFORMATION_MESSAGE);
+
         }
-
-        // writing to file
-        File file = new File(name + choice + "-" + wList.getLength() + ".txt");
-        file.createNewFile();
-        FileWriter writer = new FileWriter(file);
-        writer.write("Group "+choice+ " WAP Results for " + name + ":\n\n");
-        writer.write(wList.toString());
-        writer.flush();
-        writer.close();
-
-        // exit message
-        JOptionPane.showMessageDialog(null,
-                "Your results have been saved to " + name + choice + "-" + wList.getLength() + ".txt.\nThank you for participating!",
-                "WAP", JOptionPane.INFORMATION_MESSAGE);
-
-    }
 }
